f1c6fc98708fab670ea77ae58ac12d0c7e937a86
Guillaume <noel.guillaume@live.fr>
2019-11-26 17:40:57 +0100

    update last_seen

--- src/components/OnlineUsers.vue
+++ src/components/OnlineUsers.vue
@@ -1,29 +1,49 @@
 <template>
   <div class="sliderMenu grayBgColor">
-    <div class="sliderHeader">
-      Online users - {{ online_list.length }}
-    </div>
+    <div class="sliderHeader">Online users - {{ online_list.length }}</div>
     <div class="userInfo" v-for="user in online_list" v-bind:key="user.user.name">
       <div class="userImg">
         <i class="far fa-user" />
       </div>
-      <div class="userName">
-        {{ user.user.name }}
-      </div>
+      <div class="userName">{{ user.user.name }}</div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        online_list: [
-          { user: { name: "someUser1" }},
-          { user: { name: "someUser2" }}
-        ]
-      };
-    },
+import gql from "graphql-tag";
+export default {
+  data() {
+    return {
+      online_list: [
+        { user: { name: "someUser1" } },
+        { user: { name: "someUser2" } }
+      ]
+    };
+  },
+  mounted() {
+    const UPDATE_LASTSEEN_MUTATION = gql`
+      mutation updateLastSeen($now: timestamptz!) {
+        update_users(where: {}, _set: { last_seen: $now }) {
+          affected_rows
+        }
+      }
+    `;
+    setInterval(
+      function() {
+        this.$apollo
+          .mutate({
+            mutation: UPDATE_LASTSEEN_MUTATION,
+            variables: {
+              now: new Date().toISOString()
+            }
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }.bind(this),
+      30000
+    );
   }
-
+};
 </script>
